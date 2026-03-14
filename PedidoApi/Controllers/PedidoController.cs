a6c9be9871c8abbed5f387015ee7f6a38e5d9837
juniorapeles <elijunior.apeles2005@gmail.com>
2026-03-14 17:47:57 -0300

    feat: adição de controller para pedido

--- PedidoApi/Controllers/PedidoController.cs
+++ PedidoApi/Controllers/PedidoController.cs
@@ -6,8 +6,30 @@ using PedidoDomain.Models;
 namespace PedidoApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[controller]")] 
 public class PedidoController : ControllerBase
 {
     
+    private readonly PedidoDbContext _context;
+    
+    public PedidoController(PedidoDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Pedido>>> GetPedidos()
+    {
+        return await _context.Pedidos.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Pedido>> CriarPedido(Pedido Pedido)
+    {
+        _context.Pedidos.Add(Pedido);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetPedidos), new { id = Pedido.Id }, Pedido);
+    }
 }
