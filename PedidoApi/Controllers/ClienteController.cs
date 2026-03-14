17c78023faea988115a2f37ce9f4519c847ed353
juniorapeles <elijunior.apeles2005@gmail.com>
2026-03-14 17:40:07 -0300

    feat: adição de controller clientes

--- PedidoApi/Controllers/ClienteController.cs
+++ PedidoApi/Controllers/ClienteController.cs
@@ -1,6 +1,35 @@
-﻿namespace PedidoApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PedidoDomain.Data;
+using PedidoDomain.Models;
 
-public class ClienteController
+namespace PedidoApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] 
+public class ClienteController : ControllerBase
 {
     
+    private readonly PedidoDbContext _context;
+    
+    public ClienteController(PedidoDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+    {
+        return await _context.Clientes.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Cliente>> CriarCliente(Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetClientes), new { id = cliente.Id }, cliente);
+    }
 }
